84a9094e5c6ef52f050f4fd3ca8e45ecb056c917
raphaelprados <santosapraphael@gmail.com>
2024-09-29 15:43:52 -0400

    Desenvolvendo base

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const app = Vue.createApp({
+    created() {
+        fetch("./sample.json")
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error
+                        (`HTTP error! Status: ${res.status}`);
+                }
+            })
+            .then((data) =>
+                console.log(data))
+            .catch((error) =>
+                console.error("Unable to fetch data:", error));
+        
+    },
+    data() {
+        return {
+            conteudo: '',
+            atividade: 0,
+            linguagem: ''
+        }
+    },
     template: '<h2>I am the template</h2>'
 })
 
